85fccbdc0fbf1865fca7b2430e57faa1bac99377
Rafael Otero <josmanmx@hotmail.com>
2019-04-15 17:24:43 -0400

    estilos del footer

---
+++ wp-content/themes/elhada3d/footer.php
@@ -0,0 +1,75 @@
+</main> <!-- #main -->
+
+<footer class="footer">
+	<div class="container">		
+		<div class="row">
+			<div class="footer-left col-xs-12 col-md-3">
+				<img class="img" src="https://fakeimg.pl/158x86" alt="">
+				<img class="img" src="https://fakeimg.pl/158x86" alt="">
+				<div class="footer-donors">
+					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
+					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
+					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
+					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
+					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
+				</div>
+			</div>
+			<div class="footer-trayectory col-xs-12 col-md-6">
+				<h3 class="footer-trayectory__title">
+					Trayectoria
+				</h3>
+				<div class="trayectory-icons">
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">
+				</div>
+			</div>
+			<div class="footer-right col-xs-12 col-md-3">
+				<div class="sponsors">
+					<h4 class="sponsors-title">Patrocinios</h4>
+					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
+					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
+				</div>
+				<div class="contact-for-events">
+					<h4 class="contact-for-events__title">
+						Pautar Eventos
+					</h4>
+					<p class="contact-for-events__info">
+						Contacta con mi mánager: <br>
+						Leonardo vera <br>
+						+58 412 071 51 23 <br>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="footer-copyright  col-md-12 col-xs-12">
+				<p>Desarrollado por 
+        <a href="https://jostech.com.ve" target="_blank">
+        JosTechS.com.ve</a>
+        <span> | </span> Hosting por 
+          <a href="https://redbyte.com.mx" target="_blank">
+          redbyte.com.mx</a>
+					<span> | </span>
+					Todos los derechos reservados 2019</p>
+			</div>
+		</div>
+	</div>
+</footer>
+<?php 
+wp_footer(); 
--- wp-content/themes/elhada3d/index.php
+++ wp-content/themes/elhada3d/index.php
@@ -116,72 +116,7 @@
 	</div>
 	<!-- <img src="https://fakeimg.pl/700?text=aqui-va%0ASusana" alt=""> -->
 </div>
-<footer class="footer">
-	<div class="container">		
-		<div class="row">
-			<div class="footer-left col-xs-12 col-md-3">
-				<img class="img" src="https://fakeimg.pl/158x86" alt="">
-				<img class="img" src="https://fakeimg.pl/158x86" alt="">
-				<div class="footer-donors">
-					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
-					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
-					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
-					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
-					<img class="donor-img" src="https://fakeimg.pl/60" alt="">
-				</div>
-			</div>
-			<div class="footer-trayectory col-xs-12 col-md-6">
-				<h3 class="footer-trayectory__title">
-					Trayectoria
-				</h3>
-				<div class="trayectory-icons">
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="trayectory-img" src="https://fakeimg.pl/60" alt="">
-				</div>
-			</div>
-			<div class="footer-right col-xs-12 col-md-3">
-				<div class="sponsors">
-					<h4 class="sponsors-title">Patrocinios</h4>
-					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
-					<img class="sponsor-img" src="https://fakeimg.pl/60" alt="">		
-				</div>
-				<div class="contact-for-events">
-					<h4 class="contact-for-events__title">
-						Pautar Eventos
-					</h4>
-					<p class="contact-for-events__info">
-						Contacta con mi mánager: <br>
-						Leonardo vera <br>
-						+58 412 071 51 23 <br>
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="footer-copyright  col-md-12 col-xs-12">
-				<p>Desarrollo y hosting www.redbyte.com.mx 
-					<span> | </span>
-					Todos los derechos reservados 2019</p>
-			</div>
-		</div>
-	</div>
-</footer>
+
 
 <?php
 get_footer();
