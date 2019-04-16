ff7a4c9a73fdd718adf84803169456aafa384ff9
Rafael Otero <josmanmx@hotmail.com>
2019-04-16 16:08:55 -0400

    Ocultando temporalmente formulario y colocando version 1.0.0

--- wp-content/themes/elhada3d/index.php
+++ wp-content/themes/elhada3d/index.php
@@ -6,7 +6,7 @@
 			<div class="col-xs-12">
 				<?php get_template_part( 'events', 'loop' ) ?> 
 			</div>
-			<div class="col-xs-12">
+			<!-- <div class="col-xs-12">
 				<form class="newsletter-form">
 					<p class="newsletter-text">¡ey!... ¡entérate de los próximos eventos!</p>
 					<p class="newsletter-title">Suscríbete a mi boletín</p>
@@ -20,7 +20,7 @@
 						</a>
 					</label>
 				</form>
-			</div>
+			</div> -->
 		</div>
 	</div>
 	<!-- <img src="https://fakeimg.pl/700?text=aqui-va%0ASusana" alt=""> -->
